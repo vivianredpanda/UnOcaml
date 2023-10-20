4ed291a51ab4d70ac87ddfc4ad43ae091b63d6fb
Ava Slade <aes372@cornell.edu>
2023-10-20 15:47:42 -0400

    Add Deck.draw_n function
    - Added function Deck.draw_n
    - Updated interface
    - Updated Deck.deal implementation to use draw_n

--- src/deck.ml
+++ src/deck.ml
@@ -10,11 +10,12 @@ module type Deck = sig
 
   val to_list : t -> Card.card list
   val of_list : Card.card list -> t
+  val reset : unit -> t
   val remove : Card.card -> t -> t
   val draw : t -> Card.card * t
+  val draw_n : t -> int -> t * Card.card list
   val deal : t -> t * Card.card list
   val is_empty : t -> bool
-  val reset : unit -> t
   val size : t -> int
 end
 
@@ -23,6 +24,7 @@ module Deck : Deck = struct
 
   let to_list (deck : t) = deck
   let of_list (lst : Card.card list) = lst
+  let reset () : t = failwith "unim"
 
   let rec remove (card : Card.card) (deck : t) =
     match deck with
@@ -35,22 +37,27 @@ module Deck : Deck = struct
 
   (** Given a list of cards [lst], a deck of cards [deck], and a counter [n],
       removes [n] cards from [deck]. Returns a pair of the updated deck and a
-      list containing the removed cards and the elements of [lst]. Requires the
-      size of [deck] is at least [n] and [n] >= 0. *)
+      list containing the removed cards and the elements of [lst]. Resets the
+      deck if the input deck didn't have enough cards. Requires [n] >= 0. *)
   let rec deal_helper (lst : Card.card list) (deck : t) (n : int) =
     match n with
     | 0 -> (deck, lst)
-    | _ ->
-        let card, new_deck = draw deck in
-        deal_helper (card :: lst) new_deck (n - 1)
+    | _ -> (
+        match deck with
+        | [] ->
+            let card, new_deck = draw (reset ()) in
+            deal_helper (card :: lst) new_deck (n - 1)
+        | _ ->
+            let card, new_deck = draw deck in
+            deal_helper (card :: lst) new_deck (n - 1))
 
-  let deal (deck : t) = deal_helper [] deck 7
+  let draw_n (deck : t) (n : int) = deal_helper [] deck n
+  let deal (deck : t) = draw_n deck 7
 
   let is_empty (deck : t) : bool =
     match to_list deck with
     | [] -> true
     | _ -> false
 
-  let reset () : t = failwith "unim"
   let size (deck : t) = List.length deck
 end
--- src/deck.mli
+++ src/deck.mli
@@ -9,6 +9,9 @@ module type Deck = sig
   val of_list : Card.Card.card list -> t
   (** Convert a list of items into a deck. *)
 
+  val reset : unit -> t
+  (** Returns the default deck with 108 cards *)
+
   val remove : Card.Card.card -> t -> t
   (** Remove a card from the given deck. Returns the updated deck. Raises
       Invalid_argument if card is not in the deck or deck is empty. *)
@@ -17,15 +20,18 @@ module type Deck = sig
   (** Draw a card from the deck. Returns the card drawn and the updated deck.
       Requires the given deck is non-empty. *)
 
+  val draw_n : t -> int -> t * Card.Card.card list
+  (** Draw n cards from the deck. Returns a list of the cards drawn and the
+      updated deck. If the given deck does not contain at least n cards, first
+      draws as many cards from the deck as possible, then resets the deck and
+      draws the rest. *)
+
   val deal : t -> t * Card.Card.card list
   (** Draws 7 cards from the deck. Returns the new deck and a list of 7 cards.
       Requires the deck contains at least 7 cards. *)
 
   val is_empty : t -> bool
   (** Returns whether or not the deck is empty. *)
-
-  val reset : unit -> t
-  (** Returns the default deck with 108 cards *)
 
   val size : t -> int
   (** Returns the number of items in the deck. *)
