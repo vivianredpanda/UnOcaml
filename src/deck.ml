1d300034d011c2749bc5a9a9098018914a135d44
Vivian <vyh5@cornell.edu>
2023-10-20 23:28:12 -0400

    Implemented Deck.reset() - Created helper methods Deck.create_given_cards, create_num_cards - Replaced Deck.reset() starter code with working initial deck

--- src/deck.ml
+++ src/deck.ml
@@ -24,12 +24,60 @@ module Deck : Deck = struct
 
   let to_list (deck : t) = deck
   let of_list (lst : Card.card list) = lst
-  let reset () : t = failwith "unim"
+
+  (* Creates [num] number of cards of the given color [color] with the string
+     version of the card type [card_type] and value [num] except when num < 0.
+     Returns [list] of all of the created cards *)
+  let rec create_given_cards (color : string) (card_type : string) (num : int)
+      (value : int) (list : Card.card list) : Card.card list =
+    let card_to_add =
+      if value < 0 then Card.to_card (color ^ " " ^ card_type)
+      else Card.to_card (color ^ " " ^ card_type ^ " " ^ string_of_int value)
+    in
+    match num with
+    | 0 -> list
+    | _ ->
+        create_given_cards color card_type (num - 1) value (card_to_add :: list)
+
+  (* Creates [value]*2 + 1 cards of type Number with the given color [color]
+     such that there is one card of value 0 and 2 cards each of value 1-n.
+     Returns [list] of all of the created cards *)
+  let rec create_num_cards (color : string) (value : int)
+      (list : Card.card list) : Card.card list =
+    match value with
+    | 0 -> create_given_cards color "" 1 value list
+    | _ ->
+        create_num_cards color (value - 1)
+          (create_given_cards color "" 2 value list)
+
+  (* Creates 19 cards of type Number of each color such that there is 1 card of
+     value 0 and 2 cards of value 1-9 for each color, 2 Skip of each color, 2
+     Reverse cards of each color, two Draw cards of each color, 4 Wildcards, and
+     4 Wildcard4 *)
+  let reset () : t =
+    create_num_cards "Blue" 9 []
+    |> create_num_cards "Green" 9
+    |> create_num_cards "Yellow" 9
+    |> create_num_cards "Red" 9
+    |> create_given_cards "Blue" "Skip" 2 (-1)
+    |> create_given_cards "Green" "Skip" 2 (-1)
+    |> create_given_cards "Yellow" "Skip" 2 (-1)
+    |> create_given_cards "Red" "Skip" 2 (-1)
+    |> create_given_cards "Blue" "Reverse" 2 (-1)
+    |> create_given_cards "Green" "Reverse" 2 (-1)
+    |> create_given_cards "Yellow" "Reverse" 2 (-1)
+    |> create_given_cards "Red" "Reverse" 2 (-1)
+    |> create_given_cards "Blue" "Plus" 2 2
+    |> create_given_cards "Green" "Plus" 2 2
+    |> create_given_cards "Yellow" "Plus" 2 2
+    |> create_given_cards "Red" "Plus" 2 2
+    |> create_given_cards "Any" "Wildcard" 4 (-1)
+    |> create_given_cards "Any" "Wildcard4" 4 (-1)
 
   let rec remove (card : Card.card) (deck : t) =
     match deck with
     | [] -> raise (Invalid_argument "card not in deck")
-    | h :: t -> if h = card then t else remove card t
+    | h :: t -> if h = card then t else h :: remove card t
 
   let draw (deck : t) =
     let card = List.nth deck (Random.int (List.length deck)) in
