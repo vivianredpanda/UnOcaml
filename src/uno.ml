d568ac39c4991da69ec98123a53a42a8e03925b9
Sophia Wang <sw895@cornell.edu>
2023-11-23 16:43:51 -0500

    Implemented drawing cards & passing
    - in uno.ml implemented drawing n # cards for wildcard4 and plus 2 cards
    - also added functionality for drawing a card (and potentially passing after drawing) for when no valid card to play

--- src/uno.ml
+++ src/uno.ml
@@ -57,7 +57,18 @@ module Game = struct
   let check_play (game : t) (card : Card.card) : bool =
     match card with
     | Wildcard _ | Wildcard4 _ -> true
-    | Skip color | Reverse color -> Card.get_color game.curr_card = color
+    | Skip color -> (
+        Card.get_color game.curr_card = color
+        ||
+        match game.curr_card with
+        | Skip _ -> true
+        | _ -> false)
+    | Reverse color -> (
+        Card.get_color game.curr_card = color
+        ||
+        match game.curr_card with
+        | Reverse _ -> true
+        | _ -> false)
     | Number (num, color) ->
         if Card.get_color game.curr_card <> color then
           match Card.get_number game.curr_card with
@@ -106,7 +117,8 @@ module Game = struct
   (* Given a list [hands], an index of that list [player], and an element
      [new_hand] to add at that index, replaces the previous element at the index
      with [new_hand] and returns the updated list. *)
-  let rec replace (hands : 'b list) (player : int) (new_hand : 'b) =
+  let rec replace (hands : 'b list) (player : int) (new_hand : 'b) : Hand.t list
+      =
     match hands with
     | [] -> []
     | h :: t ->
@@ -115,28 +127,48 @@ module Game = struct
 
   (* Given a card, a player index, and a total number of players, returns the
      index of the next player. Requires 0 <= [player] < [n] - 1. *)
-  let next_player (card : Card.card) (player : int) (n : int) =
+  let next_player (card : Card.card) (player : int) (n : int) : int =
     match card with
     | Number _ | Plus _ | Wildcard _ | Wildcard4 _ | Reverse _ ->
         if player = n - 1 then 0 else player + 1
     | Skip _ ->
         if player = n - 1 then 1 else if player = n - 2 then 0 else player + 2
 
-  (* To-do: Add non-number card functionality to determine the next_player when
-     reverse or skip is played. *)
+  (* Given a list of drawn cards and a hand to add them to, returns the new hand
+     with all the cards added. *)
+  let rec add_to_hand (cards : Card.card list) (hand : Hand.t) : Hand.t =
+    match cards with
+    | [] -> hand
+    | card :: t -> add_to_hand t (Hand.add_card card hand)
+
+  (* Given n number of cards to draw, a hand to add to, and a deck, draws n
+     cards from the deck and adds them to the given hand, then returns the new
+     deck and new hand. *)
+  let rec draw_n_cards (n : int) (hand : Hand.t) (deck : Deck.t) :
+      Deck.t * Hand.t =
+    let new_deck, drawn_cards = Deck.draw_n deck n in
+    let new_hand = add_to_hand drawn_cards hand in
+    (new_deck, new_hand)
+
+  let handle_draw (n : int) (hands : Hand.t list) (player : int) (hand : Hand.t)
+      (deck : Deck.t) : Hand.t list * Deck.t =
+    let new_deck, new_hand = draw_n_cards n hand deck in
+    (replace hands player new_hand, new_deck)
 
   (* Given a game state, the index of the current player, the current player's
      hand after playing a card, and the card played, return the list of hands of
      all the players based on the card played, and the updated deck. *)
   let update_hands (game : t) (player : int) (new_hand : 'b) (card : Card.card)
       =
+    let new_hands, new_deck =
+      (replace game.hands player new_hand, game.curr_deck)
+    in
+    let next_idx = next_player card player (List.length game.hands) in
+    let next_hand = get_hand game next_idx in
     match card with
-    | Number _ | Wildcard _ | Skip _ | Reverse _ ->
-        (replace game.hands player new_hand, game.curr_deck)
-    | _ -> failwith "Non-number card functionality unimplemented"
-  (* To-do: Add non-number card functionality to switch hands around when
-     reverse is played, and add cards to the next player's deck if Plus/
-     Wildcard4 is played. *)
+    | Number _ | Wildcard _ | Skip _ | Reverse _ -> (new_hands, new_deck)
+    | Wildcard4 _ -> handle_draw 4 new_hands next_idx next_hand new_deck
+    | Plus (n, _) -> handle_draw n new_hands next_idx next_hand new_deck
 
   let play_card (game : t) (card : Card.card) (new_hand : Hand.t) =
     let player = game.curr_player in
@@ -176,6 +208,31 @@ module Game = struct
         raise (Invalid_argument "need to specify wildcard color")
       else if card_input = "Any Wildcard4" then
         raise (Invalid_argument "need to specify wildcard4 color")
+      else if card_input = "Draw card" then
+        let new_hands, new_deck =
+          handle_draw 1 game.hands game.curr_player
+            (List.nth game.hands game.curr_player)
+            game.curr_deck
+        in
+        {
+          curr_deck = new_deck;
+          curr_card = game.curr_card;
+          curr_player = game.curr_player;
+          hands = new_hands;
+          human_index = game.human_index;
+        }
+      else if card_input = "Pass" then
+        let next_idx =
+          (List.length game.hands + game.curr_player + 1)
+          mod List.length game.hands
+        in
+        {
+          curr_deck = game.curr_deck;
+          curr_card = game.curr_card;
+          curr_player = next_idx;
+          hands = game.hands;
+          human_index = game.human_index;
+        }
       else
         let card = Card.to_card card_input in
         if check_play game card then
